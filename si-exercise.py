ed94f18ecb4b39cd8cbd2026227cd85628df03b5
Kellee Springer <kelleebellspringer@gmail.com>
2024-10-12 11:12:16 -0500

    d

--- si-exercise.py
+++ si-exercise.py
@@ -75,6 +75,5 @@ def collectLegoSets(startURL):
 startURL = "https://brickset.com/sets/year-2019"
 
 lego2019_df = collectLegoSets(startURL)
-print(f"Total sets scraped: {lego2019_df.shape[0]}")
 
 lego2019_df.to_csv('lego2019.csv', index=False)
